f63671a9a3e16b42e01d87192349237119ad5d93
MargoB2402 <106451977+MargoB2402@users.noreply.github.com>
2022-06-26 14:05:30 +0300

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const NFTCONTRACT = "0x310191165316D62d971F3c500Ed6857e094837eF";
+export const STAKINGCONTRACT = "0x29c18875d1643Df270875a78E632Cc7Fe17c0395";
+export const polygonscanapi = "https://api-testnet.polygonscan.com/api";
+export const moralisapi = "https://deep-index.moralis.io/api/v2/";
+export const nftpng = "https://ipfs.io/ipfs/QmbH8tumcAwuEDwVR2rKu95tc7iVctYjN445KyjPPD6Gaj/";
